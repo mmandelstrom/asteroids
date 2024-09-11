5a6382311454581d2fa78cad3530ed74c335db2e
mmandelstrom <mmandelstrom@gmail.com>
2024-09-11 13:54:19 +0200

    Added move method on player class to enable moving forward and backwards, added w & s keys to map to move in update method

--- main.py
+++ main.py
@@ -20,9 +20,11 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        player.update(dt)
+
         screen.fill((0, 0, 0))
         player.draw(screen)
-        player.update(dt)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
         
