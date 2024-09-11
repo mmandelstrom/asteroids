bbc41753c0723422495013664659b5a13fd4b150
mmandelstrom <mmandelstrom@gmail.com>
2024-09-11 14:07:12 +0200

    Changed gameloop to access group instead of player object directly

--- main.py
+++ main.py
@@ -13,7 +13,13 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+
 
 
     while True:
@@ -21,10 +27,15 @@ def main():
             if event.type == pygame.QUIT:
                 return
             
-        player.update(dt)
-
+        for obj in updatable:
+            obj.update(dt)
+        
         screen.fill((0, 0, 0))
-        player.draw(screen)
+
+        for obj in drawable:
+            obj.draw(screen)
+
+        
         pygame.display.flip()
         dt = clock.tick(60) / 1000
         
