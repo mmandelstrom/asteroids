673175d23c19a43ed7ad1e102a606cf1ca7f81af
mmandelstrom <mmandelstrom@gmail.com>
2024-09-11 13:09:06 +0200

    Added pygame time clock to help set fps, set up clock.Time to cap fps at 60, and set a Delta variable to catch time passed between calls in second

--- main.py
+++ main.py
@@ -6,7 +6,12 @@ def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
     screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
+    time = pygame.time.Clock()
+    dt = 0
+
+
 
     while True:
         for event in pygame.event.get():
@@ -14,6 +19,9 @@ def main():
                 return
         pygame.Surface.fill(screen, (0, 0, 0))
         pygame.display.flip()
+        time.tick(60)
+        dt = time.get_time() / 1000
+    
 
 if __name__ == "__main__":
     main()
