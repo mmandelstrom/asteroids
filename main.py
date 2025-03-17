6e1053b89849075e87291f5397d0210a219a8418
Marcus Mandelström <marcusmandelstrom1@gmail.com>
2025-03-17 20:13:28 +0100

    Created player class with draw function and player object in main

--- main.py
+++ main.py
@@ -3,6 +3,10 @@
 # throughout this file
 import pygame
 from constants import * #Static values for screen size/asteroids size and speed
+from player import Player
+
+clock = pygame.time.Clock()#Clock to track gametime
+dt = 0 #Delta time variable
 
 def main():
     pygame.init()#Initialize all pygame modules
@@ -12,14 +16,20 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    x = SCREEN_WIDTH / 2 #Static values to make player spawn in the middle of screen
+    y = SCREEN_HEIGHT / 2
+
+    player = Player(x, y)#Create player object
+
     while True:#Endless gameloop
         for event in pygame.event.get():#Create button to close window
             if event.type == pygame.QUIT:
                 return
 
         screen.fill("black")
+        player.draw(screen) #Draw player each iteration
         pygame.display.flip()#Refresh screen, should be called last in loop
-
+        dt = clock.tick(60) / 1000 #Pauses game for 1/60th of a second, sets dt to amount of time since it was last called in milliseconds
 
 
 if __name__ == "__main__":
